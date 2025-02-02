afd6989732785c0e6e02861b40472fd4e186732b
Vladimir <scobca18@yandex.ru>
2025-02-02 22:33:39 +0300

    fix(app): fixed main app layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
-  <AsideBar :is-open="true"></AsideBar>
+  <div class="app_primary_container">
+  </div>
 </template>
 
 <script setup lang="ts">
-import AsideBar from "./components/AsideBar.vue";
+
 console.log(document.documentElement.clientWidth)
 </script>
+
+<style scoped>
+.app_primary_container {
+  min-height: calc(100vh - 60px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
